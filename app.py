e399ff5953a90cb73cb844cfe0b96d403aa95ca7
Ammon Shepherd <ammon.uva@gmail.com>
2020-10-02 12:10:07 -0400

    Adds db migration abilities

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 # app.py
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Meals
 
 
 @app.route('/')
---
+++ models.py
@@ -0,0 +1,23 @@
+from app import db
+from sqlalchemy.dialects.postgresql import JSON
+
+ 
+class Meals(db.Model):
+    __tablename__ = 'mealer'
+ 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String())
+    description = db.Column(db.String())
+    ingredients = db.Column(db.String())
+    image = db.Column(db.String())
+    url = db.Column(db.String())
+ 
+    def __init__(self, title, description, ingredients, image, url):
+        self.title = title
+        self.description = description
+        self.ingredients = ingredients
+        self.image = image
+        self.url = url
+ 
+    def __repr__(self):
+        return f"{self.title}:{self.description}:{self.ingredients}:{self.image}:{self.url}"
